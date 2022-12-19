88f8ea155537481e29c7cf28dc061dcdd11db994
Mehmet Yalcin <mehmetyalcin@hotmail.com>
2022-12-19 21:14:10 +0300

    task4 complited

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/TC4_LibraryVerification.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/TC4_LibraryVerification.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TC4_LibraryVerification {
@@ -14,9 +16,15 @@ public class TC4_LibraryVerification {
         //2. Go to http://library2.cybertekschool.com/login.html
         driver.get("http://library2.cybertekschool.com/login.html");
         //3. Enter username: “incorrect@email.com”
-
+        WebElement userNameInput= driver.findElement(By.className("form-control"));
+        userNameInput.sendKeys("incorrect@email.com");
         //4. Enter password: “incorrect password”
+        WebElement passwordInput= driver.findElement(By.id("inputPassword"));
+        passwordInput.sendKeys("incorrect password");
         //5. Verify: visually “Sorry, Wrong Email or Password”
+
+        WebElement signINButton= driver.findElement(By.tagName("button"));
+        signINButton.click();
         //displayed
         //PS: Locate username input box using “className” locator
         //Locate password input box using “id” locator
