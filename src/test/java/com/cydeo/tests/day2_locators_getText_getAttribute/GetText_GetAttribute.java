fbfe4c18bc2b412a10b1bea1014226035832fb03
Mehmet Yalcin <mehmetyalcin@hotmail.com>
2022-12-19 22:19:06 +0300

    task5 complited

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/GetText_GetAttribute.java
@@ -0,0 +1,43 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetText_GetAttribute {
+    public static void main(String[] args) {
+        //TC #5: getText() and getAttribute() method practice
+        //1- Open a chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        //2- Go to: https://practice.cydeo.com/registration_form
+        driver.get("https://practice.cydeo.com/registration_form");
+        //3- Verify header text is as expected:
+
+        WebElement headerText= driver.findElement(By.tagName("h2"));
+        //Expected: Registration form
+        String expectedHeaderText= "Registration form";
+        String actualHeaderText=headerText.getText();
+
+        if (actualHeaderText.equals(expectedHeaderText)){
+            System.out.println("Header text verificitaion Passed");
+        }else {
+            System.out.println("Header text verificitaion Failed");
+        }
+        //4- Locate “firstname” input box
+        WebElement firstnameInput= driver.findElement(By.name("firstname"));
+
+        //5- Verify placeholder attribute’s value is as expected:
+        //Expected: first name
+        String expectedPlaceHolder="first name";
+        String actualPlaceHolder= firstnameInput.getAttribute("placeholder");
+        if (actualPlaceHolder.equals(expectedPlaceHolder)){
+            System.out.println("Placeholder text verification PASSED");
+        }else {
+            System.out.println("Placeholder text verification PASSED");
+        }
+        driver.close();
+    }
+}
